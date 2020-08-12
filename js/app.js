cb53b56acb1ff0463bf526a6032343fc1115dec9
Duvey <rudow.david@gmail.com>
2020-08-12 14:30:03 -0500

    table created in js

--- js/app.js
+++ js/app.js
@@ -3,3 +3,24 @@ const tableData = data;
 
 // Reference the HTML table using d3 (pointing to the html table)
 var tbody = d3.select("tbody");
+
+function buildTable(data){
+    // clear the table to make sure the data is not already filtered
+    tbody.html("");
+
+    //loop through each data entry
+    data.forEach((dataRow) => {
+        // add a row to the table
+        let row = tbody.append("tr");
+        // Loop through each field in the dataRow and add
+        // each value as a table cell (td)
+        Object.values(dataRow).forEach((val)=> {
+            // append data into a data table tag
+            let cell = row.append("td");
+            // added the value to the cell
+            cell.text(val);
+            });
+        });
+
+    
+}
